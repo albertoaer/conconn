2f59b5ed4ffd927f0d7393f83487d515c9714a10
Alberto Elorza <albertoelorza2002@gmail.com>
2023-10-07 14:15:37 +0200

    better ws bench task

--- lib/mix/tasks/bench/ws.ex
+++ lib/mix/tasks/bench/ws.ex
@@ -2,14 +2,26 @@ defmodule Mix.Tasks.Bench.Ws do
   use Mix.Task
 
   @impl Mix.Task
-  def run(args) when length(args) > 0 do
+  def run(args) do
+    {flags, argv} = OptionParser.parse!(args, strict: [traffic: :integer, clients: :integer])
+    url = extract_url(argv)
     task = Conconn.Client.WebSocket.launch(
-      Enum.at(args, 0),
+      url,
       {
         Conconn.ConcTask.PingPongConcTask,
-        traffic: Enum.at(args, 1, 1000),
-      }
+        traffic: Keyword.get(flags, :traffic, 1000),
+      },
+      Keyword.get(flags, :clients, 1)
     )
-    Conconn.Launcher.await(task)
+    Conconn.Launcher.await(task) |> Kernel.inspect() |> Mix.Shell.IO.info()
+  end
+
+  def extract_url(argv) do
+    try do
+      [url] = argv
+      url
+    rescue
+      _ -> reraise "Expecting only an argument, the url", __STACKTRACE__
+    end
   end
 end
